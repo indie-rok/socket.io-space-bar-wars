55deec069c3b6e9e21ac3e8692affff9932d4efc
Emmanuel Orozco <yo@emmanuelorozco.com>
2017-07-03 00:24:52 -0500

    implemented first draft of game data object

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ var io = require("socket.io")(http);
 var redis = require("redis");
 var client = redis.createClient();
 
+client.on('connect',()=>{
+  client.set('game:scoreToWin',10);
+  client.set('game:team-1-score',5);
+  client.set('game:team-2-score',5);
+});
+
 app.use('/public', express.static(__dirname + '/public'));
 
 app.get("/", (req,res) => {
@@ -22,9 +28,29 @@ app.get("/team2", (req,res) => {
   res.sendFile(__dirname + '/public/team2.html');
 })
 
-io.on("connection",() => {
-  console.log("connected");
+
+
+io.on("connection",(socket) => {
+
+  socket.on("team 1 press",()=>{
+    client.incr('game-team-1-score', (err,score) => {
+      io.sockets.emit('update score', getCurrentScore());
+    })
+  })
 })
+
+function getCurrentScore(){
+
+
+
+  client.get("game:scoreToWin", (err,score) => gameData.scoreToWin = score );
+
+  client.get('game:team-1-score', (err,score) => gameData.scoreA = score);
+
+  client.get('game:team-2-score', (err,score) => gameData.scoreB = score);
+
+
+};
 
 
 http.listen(8000,()=>console.log('working on 8000'));
